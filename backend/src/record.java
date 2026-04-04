b5e626521ce405911ffd0a958e882948ab552c66
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2026-04-04 21:21:09 +0900

    コード追加

---
+++ backend/src/record.java
@@ -0,0 +1,30 @@
+package record;
+
+protected class record {
+  public static void main(String[] args) {
+
+    int username = request.getParameter("username");  //目的
+    char b = 0;  //場所
+    int c = 0;  //交通手段
+
+    if ( a == 4) {  //目的がその他の場合、テキストボックスに入れるようにする
+
+      
+    }
+  }
+
+  //目的
+  public static int purpose (int a) {
+    return (a);
+  }
+
+  //場所
+  public static char place (char a) {
+    return (a);
+  }
+
+  //交通手段
+  public static int way (int a) {
+    return (a);
+  }
+}
