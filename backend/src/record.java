239eb0ac7fb8d21f8d7eea801b732c39f9b915d7
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2026-04-05 16:59:28 +0900

    送信ボタンの修正など

--- backend/src/record.java
+++ backend/src/record.java
@@ -2,29 +2,42 @@ package record;
 
 protected class record {
   public static void main(String[] args) {
+    int pur = request.getParameter("pur");  //目的
+    purpose(pur)
+    System.out.println(pur);
 
-    int username = request.getParameter("username");  //目的
-    char b = 0;  //場所
-    int c = 0;  //交通手段
+   int pla = request.getParameter("pla");  //場所
+    purpose(pla)
+    System.out.println(pla);
 
-    if ( a == 4) {  //目的がその他の場合、テキストボックスに入れるようにする
+   int way = request.getParameter("way");  //交通手段
+    purpose(way)
+    System.out.println(way);
 
-      
-    }
   }
 
   //目的
-  public static int purpose (int a) {
-    return (a);
+  public static int purpose (int pur) {
+    return (pur);
   }
 
   //場所
-  public static char place (char a) {
-    return (a);
+  public static char place (char pla) {
+    return (pla);
   }
 
   //交通手段
-  public static int way (int a) {
-    return (a);
+  public static int way (int way) {
+    return (way);
+  }
+
+  //送信
+  protected void submit (int s) {
+    if (pur < 0 && way < 0) {  //目的と交通手段に値を入れていないと送信できない
+      
+    } else {
+      System.out.println("値を入力してください");
+    }
+
   }
 }
