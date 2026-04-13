3cf25757d093ff32e9d60fbf86359f8e00f3480e
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2026-04-13 20:50:05 +0900

    JavaとDBの接続

--- backend/src/main.java
+++ backend/src/main.java
@@ -1,7 +1,25 @@
-package main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class main {
-  	public static void main(String[] args) {
-      System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://db:5432/travelloger";
+        String user = "travelloger";
+        String password = "travelloger";
+
+        String sql = "INSERT INTO users (name) VALUES (?)";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, "yuuhi_java");
+            pstmt.executeUpdate();
+
+            System.out.println("登録成功！");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
